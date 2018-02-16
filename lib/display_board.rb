03d214cc7107373baf048c2b71426ae56e7a2a8e
Emily Lazio <emily.lazio@wework.com>
2018-02-16 00:21:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,22 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  row = [#{" 0 "}, "  |   |   "}
+  middle = "-----------"
+  puts row
+  puts middle
+  puts row
+  puts middle
+  puts row
+end
+
+
+
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+display_board(board) 
+
+# puts #{0} 
+
+puts board[0]
