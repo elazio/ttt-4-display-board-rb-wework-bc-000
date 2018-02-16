e90fcee42abf7beadccb8062d7be9e71d5b0bd8e
Emily Lazio <emily.lazio@wework.com>
2018-02-16 00:48:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 # out the current state.
 
  def display_board(board)
-   puts row_one = "#{board[0]}|#{board[1]}|#{board[2]}"
+   puts row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
    puts middle = "-----------"
-   puts row_two = "#{board[3]}|#{board[4]}|#{board[5]}"
+   puts row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
    puts middle = "-----------"
-   puts row_three = "#{board[6]}|#{board[7]}|#{board[8]}"
+   puts row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
  end 
    
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
